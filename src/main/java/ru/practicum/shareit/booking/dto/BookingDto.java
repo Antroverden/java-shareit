798a0a49a574c9ced04c84008cd3ec8b2d3a3bef
Antroverden <Antroverden@yandex.ru>
2023-06-16 17:25:33 +0300

    add fixes according to review

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
