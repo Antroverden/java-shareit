da70f5b422a41c6087f5b4301ad902bbb4ba9f1f
Antroverden <Antroverden@yandex.ru>
2023-06-16 16:49:19 +0300

    fix: codestyle, add custom exceptions

--- src/main/java/ru/practicum/shareit/exception/BadRequestException.java
+++ src/main/java/ru/practicum/shareit/exception/BadRequestException.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.exception;
 
-import org.springframework.http.HttpStatus;
-
-public class BadRequestException extends RuntimeException{
+public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
         super(message);
