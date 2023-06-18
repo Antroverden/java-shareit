d93f0cf0ee0630160cf4af8a0ff750e630d0406d
Antroverden <Antroverden@yandex.ru>
2023-06-19 01:24:39 +0300

    pre-commit

--- src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
@@ -4,14 +4,11 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.request.storage.ItemRequestRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
